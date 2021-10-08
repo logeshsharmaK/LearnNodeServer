e278c38bf708ff84e33ccec9d0d966873b5cfcd3
logessharmaopenwave@gmail.com <logeshsharmaopenwave@gmail.com>
2021-10-08 17:33:08 +0530

    renamed commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.get('/', function (req, res) {
+
+    res.json({ "message": "Hi Logesh sharma we are working in node js" });
+});
+
+
+app.get('/login', function (req, res) {
+    res.json({ "message": "welcome" });
+});
+app.listen(3000, function () {
+    console.log('Server is started and listening on port 3000');
+});
